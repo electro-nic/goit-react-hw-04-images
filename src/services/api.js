474108bf5628ab36e1fafb0f93b5637edb9acb4f
Evgen Batsman <evgenbatsman@gmail.com>
2022-10-01 14:51:22 +0300

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
-const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '11753469-fae5ae7c0c549b8c5c7373ba2';
 const fetchImages = async (searchItem, page) => {
-    const response = await axios.get(`${BASE_URL}?q=${searchItem}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`);
+    const response = await axios.get(`https://pixabay.com/api/?q=${searchItem}&page=${page}&key=11753469-fae5ae7c0c549b8c5c7373ba2&image_type=photo&orientation=horizontal&per_page=12`);
     return response.data;
 }
 export default fetchImages;
